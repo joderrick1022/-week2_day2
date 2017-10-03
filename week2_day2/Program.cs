6b8358ddc269c14d9adba880b41f4bfd34660f67
joderrick1022 <joderrick1022>
2017-10-03 13:34:19 -0400

    foreach loop

--- week2_day2/Program.cs
+++ week2_day2/Program.cs
@@ -10,6 +10,55 @@ namespace week2_day2
     {
         static void Main(string[] args)
         {
+            ////for loop
+            //for(int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+            //string greetings = " Hi my name is JD";
+            //string[] words = greetings.Split();
+            //for (int i = 0; i < words.Length ; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+
+            // list favfoods throuh a loop
+
+            //string[] favFoods = { "pizza", "chicken", "burgers", "salmon", "Roast", };
+            //for(int i = 0; i < favFoods.Length; i++)
+            //{
+            //    Console.WriteLine(favFoods[i]);
+            //}
+
+
+
+
+            // lucy numbers call numers through foreach loop
+
+            //int[] luckyNum = {1,2,4,6,34,5,7,23,9, };
+
+            //foreach (int numer in luckyNum)
+            //{
+            //    Console.WriteLine("Your lucky number is :" + " "+ numer);
+            //}
+
+            // call days of the week usi
+            string days =  "mon tues wen thur fri sat sun" ;
+            string[] day = days.Split();
+           
+          // foreach(string d in day)
+            //{
+                Console.WriteLine(day[0]);
+           // }
+
+
+
         }
     }
 }
