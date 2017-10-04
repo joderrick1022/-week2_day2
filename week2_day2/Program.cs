f819568d270fcc8d46598363a8b6789527e730c7
joderrick1022 <joderrick1022>
2017-10-04 10:54:42 -0400

    the while and do whiele loop (play again)

--- week2_day2/Program.cs
+++ week2_day2/Program.cs
@@ -59,28 +59,85 @@ namespace week2_day2
             //    Console.WriteLine(day[0]);
             //}
 
-            string whatToSay = "once upon a time";
+            //string whatToSay = "once upon a time";
 
-            string[] storyWords = whatToSay.Split();
-            Array.Reverse(storyWords);
+            //string[] storyWords = whatToSay.Split();
+            //Array.Reverse(storyWords);
             //for (int i = 0; i < storyWords.Length; i++)
             //{
             //    Console.WriteLine(storyWords[i]);
             //}
 
+            // lesson above using foreach loop
+            //foreach(string i in storyWords){
+            //    Console.WriteLine(i);
+            //}
 
-            foreach(string i in storyWords){
-                Console.WriteLine(i);
+            //this is forfor each loop to count backwards
+            //for(int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            //this is the while loop
+            //this is the way you make a question reapear.
+            //Console.WriteLine(  "do yoe want to playy again");
+            //string playAgain = Console.ReadLine();
+            //while (playAgain == "yes")
+            //{
+            //    Console.WriteLine("its a rematch");
+            //    Console.WriteLine("do you want to play again? (yes/No)");
+            //    playAgain = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("Hello would you like to check patient in for appointment (yes/no)");
+            //string checIn = Console.ReadLine();
+            //Console.WriteLine(checIn.ToLower());
+
+
+
+
+            //while (checIn == "yes" ) 
+            //{
+
+
+            //    Console.WriteLine( "Patient Check IN System");
+            //    Console.WriteLine(  "What is your full name EX first/last)");
+
+            //   string fullName = Console.ReadLine();
+            //    Console.WriteLine("what is patient six digit patient Id ex(000000)");
+            //   int id = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("what time is your appointment Ex(1:00)");
+            //        string time = Console.ReadLine();
+            //    Console.WriteLine("do you want to check patient in for appointment (yes/no)");
+            //    string appointment = Console.ReadLine();
+
+
+
+            //}
+
+
+            // do while
+            // do while loop is good for yes and no questions
+            string playAgain;
+
+
+            do
+            {
+                
+                Console.WriteLine("Great game");
+                Console.WriteLine("Do you want to play again?");
+                playAgain = Console.ReadLine();
+                Console.WriteLine(playAgain.ToLower());
             }
+            while (playAgain == "yes");
 
+            Console.WriteLine("thanks for playing");
+            {
 
-
-            
-
-
-
-
-
+            }
 
         }
     }
